95cd02c241a53be047bb9c5378f2eb4cc42ce77a
Alain Nshimirimana <alain.nshimirimana@gmail.com>
2021-01-06 22:55:14 -0800

    I'm able to add items to the todo list and display the entire list everytime I add something. Next is to addd a function that let you edit the item that was add to the list

---
+++ TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_To_Do_List_App
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
